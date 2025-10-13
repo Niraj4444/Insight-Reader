3d344d205877d1e41d3d1bf401f0e19a4761c7da
Niraj <136625649+Niraj4444@users.noreply.github.com>
2025-10-13 07:24:39 +0000

    dd

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "digital-library-b.firebaseapp.com",
   projectId: "digital-library-b",
-  storageBucket: "digital-library-b.firebasestorage.app",
+  storageBucket: "digital-library-b.appspot.com",
   messagingSenderId: "587099881192",
   appId: "1:587099881192:web:e317fc9927f8e2044e535d",
   measurementId: "G-LCHJ7SW6NZ"
