934636a7a5cdbba3f09d0013590a00e3b49e2941
Niraj <136625649+Niraj4444@users.noreply.github.com>
2025-10-13 13:09:40 +0000

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ IMPORTANT: fixes JS load on Vercel
   server: {
     host: '0.0.0.0',
-    // ADD THIS PROPERTY
     allowedHosts: ['.replit.dev']
   }
-})
+});
